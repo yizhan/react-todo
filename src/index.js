c943c23288f7850449ecd5fb0bd049175ef08839
Robert Wang <robertw@affinityteam.com>
2023-02-03 13:46:07 +1300

    comment app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import HelloBlack from './component/HelloBlack';
 import HelloWhite from './component/HelloWhite';
 
